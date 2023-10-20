3ced97274ee04ad964ac9a87d29f71f5c66eb2eb
seohaneul <seo1533618@gmail.com>
2023-10-20 19:54:47 +0900

    FIX : 테이블 수정

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -2,34 +2,101 @@ import React, { useEffect, useState } from 'react'
 import Button from '../components/Button'
 import styled from 'styled-components'
 import axios from 'axios';
+import Add from '../components/Add';
 
 export default function Main() {
-    const [tableMoney, setTableMoney] = useState(0);
-    const [tableDetail, setTableDetail] = useState("");
+    const [tableMoney, setTableMoney] = useState([]);
+    const [tableDetail, setTableDetail] = useState([]);
+    let moneyArr = [];
+    let detailArr = [];
 
     useEffect(() => {
         axios.get('/api/products')
             .then(res => {
-                console.log(res.data.product);
-                setTableMoney(res.data.product[0].money);
-                setTableDetail(res.data.product[0].detail);
+                // console.log(res.data.product);
+                let arr = res.data.product;
+                arr.forEach((el, item) => {
+                    moneyArr[item] = el.money;
+                    detailArr[item] = el.detail;
+                    // console.log(detailArr.length)
+                    setTableMoney(moneyArr);
+                    setTableDetail(detailArr);
+                });
             })
     }, []);
 
+
+
     return (
         <>
-            <Body>
+            <Info>
                 <Button />
-                <h3>MONEY : {tableMoney}</h3>
-                <h3>DETAIL : {tableDetail}</h3>
-            </Body>
+                <Table>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>MONEY</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                tableMoney.map((number, idx) => {
+                                    return (
+                                        <tr>
+                                            <td>{number}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>MONEY</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                tableDetail.map((number, idx) => {
+                                    return (
+                                        <tr>
+                                            <td>{number}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+
+                        </tbody>
+                    </table>
+                </Table>
+            </Info>
+            {/* <Add /> */}
         </>
     )
 }
 
-const Body = styled.div`
+const Info = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+`
+
+const Table = styled.div`
+    display: flex;
+    
+    >table{
+        margin: 10px;
+        >tbody{
+            >tr{
+                >td{
+                    padding: 10px;
+                    text-align: center;
+                    border-bottom: 1px solid #000;
+                }
+            }
+        }
+    }
 `
