07de396f1a7bfefd9311a7a5bd0ff9a4d6253aac
seohaneul <seo1533618@gmail.com>
2023-10-20 19:53:46 +0900

    DELETE : 주석 제거

--- server/app.js
+++ server/app.js
@@ -5,32 +5,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 const db = require("./routes/index");
 
-// app.use(
-//   cors({
-//     origin: "*",
-//     credentials: true,
-//     optionsSuccessStatus: 200,
-//   })
-// );
-
-// app.use(express.urlencoded({ extended: true }));
-
 app.listen(port, () => {
   console.log(`http://localhost:${port}/api/products`);
 });
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.get("/api/products", (req, res) => {
   db.query("SELECT * FROM FOUNDS;", (err, data) => {
